9b670cf5f24677c3abf4e662a685b5e57411f4d8
JOÃO PEDRO MAXIMINIANO DE OLIVEIRA <54539938800@info.local>
2026-05-25 17:15:06 -0300

    Instânciando Objeto Lampada e criando estrurtura de const int

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <Arduino.h>
+#include <Lampada.h>
 
+Lampada interruptor1;
+Lampada interruptor2;
+Lampada interruptor3;
+Lampada interruptor4;
 
+const int pinoInterruptor1 = 5;
+const int pinoInterruptor2 = 6;
+const int pinoInterruptor3 = 7;
+const int pinoInterruptor4 = 8;
 
 void setup() {
  
